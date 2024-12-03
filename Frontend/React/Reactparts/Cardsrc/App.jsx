df6e361c44fc8033bdf30caddfd287dcaad6a879
sonalimuduli <sonalimuduli78gmail.com>
2024-12-04 12:33:01 +1300

    project

---
+++ Frontend/React/Reactparts/Cardsrc/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  
+
+  return (
+    <>
+    <Card namme ={'sonali'} age={21} collage={'usbm'}/>
+    <Card namme ={'ankita'} age={21} collage={'usbm'}/>
+    <Card namme ={'sudi'} age={21} collage={'usbm'}/>
+
+    </>
+  )
+}
+
+export default App
+//to create  a profile card(name,age,role)
---
+++ Frontend/React/Reactparts/Cardsrc/components/Card.jsx
@@ -0,0 +1,16 @@
+
+
+
+const Card = ({name,age,collage}) => {
+  return  (
+    <>
+    my name is(name)
+    I am {age} year old
+    studing at {collage}
+
+    </>
+    
+  )
+}
+export default Card;
+
